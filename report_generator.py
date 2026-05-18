1cd18838b80640a6f0aba68487fb1142a0e7dad1
iirasubizadivine <iirasubizadivine@gmail.com>
2026-05-18 10:12:27 +0300

    Add files via upload

---
+++ report_generator.py
@@ -0,0 +1,42 @@
+def calculate_average(backend, frontend, design):
+    return (backend + frontend + design) / 3
+
+
+def get_grade(average):
+    if average >= 80:
+        return "A"
+    elif average >= 70:
+        return "B"
+    elif average >= 60:
+        return "C"
+    elif average >= 50:
+        return "D"
+    else:
+        return "E"
+
+
+def create_report(name, backend, frontend, design):
+    avg = calculate_average(backend, frontend, design)
+    grade = get_grade(avg)
+
+
+    return {
+        "name": name,
+        "Backend": backend,
+        "Frontend": frontend,
+        "Design": design,
+        "average": int(avg) if avg.is_integer() else round(avg, 2),
+        "grade": grade,
+    }
+
+
+
+name = input("Enter student name: ")
+backend = float(input("Enter Backend marks: "))
+frontend = float(input("Enter Frontend marks: "))
+design = float(input("Enter Design marks: "))
+
+
+report = create_report(name, backend, frontend, design)
+print("Report: ")
+print(report)
